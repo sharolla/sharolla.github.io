56e601512d98be551a21224073fdce51029d72df
Dmyjm <bgdm1260263@gn.iwasaki.ac.jp>
2017-10-23 14:47:02 +0900

    LocalStorage

--- scfesh_storage_manage.js
+++ scfesh_storage_manage.js
@@ -1,4 +1,10 @@
+// お気に入りフレーズ登録用ローカルストレージ
+// 容量制限を考え、50フレーズまで登録可とする
+// key: ID, value: フレーズ日英
+// Twitter: @Ringtonix
+
+// ローカルストレージ使えるならGO！
 if(window.localStorage){
-    var local_storage = window.localStorage;
-    console.log(local_storage);
+    var fraseStorage = window.localStorage;
+    console.log("Frases: " + fraseStorage.length);
 }
